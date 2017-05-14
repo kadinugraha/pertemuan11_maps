bcf09b18975b389ee33ad4b1bae98cf58495cd5b
Kristian Adi Nugraha <adinugraha@ti.ukdw.ac.id>
2017-05-14 12:00:11 +0700

    first commit

---
+++ app/src/main/java/id/ac/ukdw/pertemuan11b_maps/MapsActivity.java
@@ -0,0 +1,91 @@
+package id.ac.ukdw.pertemuan11b_maps;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private Marker myMarker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //setting GPS
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = lm.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = lm.getLastKnownLocation(provider);
+        if(location != null)
+            onLocationChanged(location);
+
+        lm.requestLocationUpdates(provider,1000,1,this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        if(myMarker == null){
+            myMarker = mMap.addMarker(
+                    new MarkerOptions()
+                            .position(myLocation)
+                            .title("Lokasi Saya")
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+            );
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        }else{
+            myMarker.setPosition(myLocation);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
